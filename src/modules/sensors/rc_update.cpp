b476906c0a5b576dfb0cf95ee1e440604160eafc
Alberto <aruizgarcia-1@tudelft.nl>
2021-06-07 16:16:54 +0200

    Fixed rc_update.cpp (default value of FW_YR_FF)

--- src/modules/sensors/rc_update.cpp
+++ src/modules/sensors/rc_update.cpp
@@ -529,8 +529,8 @@ RCUpdate::rc_poll(const ParameterHandles &parameter_handles)
                 float param_1 = get_rc_value(rc_channels_s::RC_CHANNELS_FUNCTION_PARAM_1, 0.0, 1.0);
                 float param_2 = get_rc_value(rc_channels_s::RC_CHANNELS_FUNCTION_PARAM_2, 0.0, 1.0);
                 // Compute gains
-                float yaw_rate_gain_new = 0.005 + 0.5*sin(M_PI_2 * (double)param_1);
-                float yaw_feedforward_gain_new = 1.2*sin(M_PI_2 * (double)param_2);
+                float yaw_rate_gain_new = 0.005 + 0.5 * sin(M_PI_2 * (double)param_1);
+                float yaw_feedforward_gain_new = sin(M_PI_2 * (double)param_2);
 
                 // Set parameters
                 if (yaw_rate_gain_new > _yaw_rate_gain + _yaw_rate_gain_sensitivity ||
